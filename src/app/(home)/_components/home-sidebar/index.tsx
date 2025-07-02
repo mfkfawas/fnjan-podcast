519d92d127aeddc975cdaf3157a8b5e48c7e9a57
mfkfawas <mfkfawas@gmail.com>
2025-07-02 04:13:51 +0300

    refactor(sidebar): refactor HomeSidebar component structure for clarity

--- src/app/(home)/_components/home-sidebar/index.tsx
+++ src/app/(home)/_components/home-sidebar/index.tsx
@@ -17,59 +17,70 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export const HomeSidebar = () => {
-  const pathname = usePathname();
-
   return (
     <Sidebar className="border-gray-800" collapsible="icon">
       <SidebarContent className="bg-background">
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <HomeSidebarHeader />
-
-              <SidebarMenuItem>
-                <SidebarMenuButton
-                  isActive={pathname === "/"}
-                  tooltip="Logo"
-                  asChild
-                >
-                  <Link href="/">
-                    <Globe className="size-5" />
-                    <span className="text-sm">Home</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-
-        <SidebarGroup>
-          <SidebarGroupLabel className="font-bold">
-            YOUR STUFF
-          </SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton isActive={false} tooltip="Logo" asChild>
-                  <Link href="/content">
-                    <Globe className="size-5" />
-                    <span className="text-sm">Content</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-
-              <SidebarMenuItem>
-                <SidebarMenuButton tooltip="Exit Studio" asChild>
-                  <Link href="/exit">
-                    <Globe className="size-5" />
-                    <span className="text-sm">Exit Studio</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <MainGroup />
+        <PersonalizedGroup />
       </SidebarContent>
     </Sidebar>
+  );
+};
+
+const MainGroup = () => {
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          <HomeSidebarHeader />
+
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              isActive={pathname === "/"}
+              tooltip="Logo"
+              asChild
+            >
+              <Link href="/">
+                <Globe className="size-5" />
+                <span className="text-sm">Home</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
+
+const PersonalizedGroup = () => {
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel className="font-bold">YOUR STUFF</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton isActive={false} tooltip="Logo" asChild>
+              <Link href="/content">
+                <Globe className="size-5" />
+                <span className="text-sm">Content</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+
+          <SidebarMenuItem>
+            <SidebarMenuButton tooltip="Exit Studio" asChild>
+              <Link href="/exit">
+                <Globe className="size-5" />
+                <span className="text-sm">Exit Studio</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
   );
 };
