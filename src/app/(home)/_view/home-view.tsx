0a760d13a9859447725c1eeb1ed70a1e071a668e
mfkfawas <mfkfawas@gmail.com>
2025-07-02 22:11:21 +0300

    feat(home): update HomeView layout and enhance podcast display

--- src/app/(home)/_view/home-view.tsx
+++ src/app/(home)/_view/home-view.tsx
@@ -2,6 +2,7 @@
 
 import { PodcastSearchSuccessResponse } from "@/app/api/search/types";
 import { CardCarousel } from "@/components/card-carousel";
+import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -35,18 +36,45 @@ export function HomeView() {
     );
 
   return (
-    <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+    <div className="max-w-[2400px] mx-auto mb-28 px-4 pt-2.5 flex flex-col gap-y-6">
       <h2 className="font-semibold py-6 border-b border-foreground/25">
         Top podcast for {query}
       </h2>
       <CardCarousel
-        data={results.map((res) => ({
+        data={results.slice(0, 14).map((res) => ({
           authorName: res.artistName,
           url: res.artworkUrl600,
           primaryGenre: res.primaryGenreName,
         }))}
         onSelect={() => {}}
       />
+
+      <h2 className="font-semibold py-6 border-b border-foreground/25">
+        All podcast for {query}
+      </h2>
+
+      <div className="flex flex-wrap">
+        {results.slice(14).map((res) => (
+          <div
+            key={res.trackId}
+            className="w-full sm:w-1/2 md:w-1/3 hover:bg-black border-b border-foreground/25 py-2 flex gap-2 items-center flex-shrink-0"
+          >
+            <Image
+              src={res.artworkUrl600 as string}
+              alt={res.artistName}
+              width={50}
+              height={50}
+            />
+
+            <div className="flex flex-col gap-2">
+              <span className="text-xs font-semibold">{res.artistName}</span>
+              <span className="text-xs text-sidebar-accent-foreground font-semibold">
+                {res.primaryGenreName}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
